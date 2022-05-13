d24b31891eefc285895ec1d323a024d348d6caa4
dm-mrtnvch <dmvshn@gmail.com>
2022-05-14 02:50:43 +0400

    next

--- src/index.ts
+++ src/index.ts
@@ -47,7 +47,7 @@ app.get('/bloggers/:id', (req: Request, res: Response) => {
     if (blogger) {
         res.status(200).send(blogger)
     } else {
-        res.send(bloggerId)
+        res.status(200).send(req.params)
     }
 })
 
