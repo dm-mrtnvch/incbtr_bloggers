6b162fc9beb91ff33571df68a8e82359b050e39a
dm-mrtnvch <dmvshn@gmail.com>
2022-05-14 02:38:55 +0400

    next

--- src/index.ts
+++ src/index.ts
@@ -20,22 +20,13 @@ let bloggers: Iblogger[] = [
     {id: 5, name: 'piat', youtubeUrl: 'https://www.youtube.com/piat'}
 ]
 
-app.get('/', (req, res) => {
-    res.send('simple get request')
-})
-app.get('/bloggers', (req: Request, res: Response) => {
-    res.status(200).send(bloggers)
-})
-app.get('/bloggers/:id', (req: Request, res: Response) => {
-    const bloggerId = +req.params.id
-    const blogger = bloggers.find(b => b.id === bloggerId)
-    console.log(blogger)
-    if (blogger) {
-        res.status(200).send(blogger)
-    } else {
-        res.send(404)
-    }
-})
+// app.get('/', (req, res) => {
+//     res.send('simple get request')
+// })
+// app.get('/bloggers', (req: Request, res: Response) => {
+//     res.status(200).send(bloggers)
+// })
+
 app.post('/bloggers', (req: Request, res: Response) => {
     if (!req.body?.name?.trim() ||  !req.body?.youtubeUrl?.trim()) {
         res.status(400).send({"errorsMessages": [{"message": "string", "field": "string"}], "resultCode": 0})
@@ -50,29 +41,40 @@ app.post('/bloggers', (req: Request, res: Response) => {
     res.status(201).send(bloggers)
 })
 
-app.put('/bloggers/:id', (req: Request, res: Response) => {
-     if (!req.body.name || !req.body.youtubeUrl) {
-        res.send(400)
-    }
+app.get('/bloggers/:id', (req: Request, res: Response) => {
     const bloggerId = +req.params.id
     const blogger = bloggers.find(b => b.id === bloggerId)
     if (blogger) {
-        blogger.name = req.body.name
-        blogger.youtubeUrl = req.body.youtubeUrl
-        res.status(204).send(blogger)
+        res.status(200).send(blogger)
     } else {
-        res.status(404).send({errorsMessages: [{message: "string", field: "title"}], resultCode: 1})
+        res.send(401)
     }
 })
-app.delete('/bloggers/:id', (req, res) => {
-    const bloggerId = +req.params.id
-    const filteredBloggers = bloggers.filter(b => b.id !== bloggerId)
-    if(filteredBloggers){
-        res.status(204).send(filteredBloggers)
-    } else {
-        res.send(404)
-    }
-})
+
+
+// app.put('/bloggers/:id', (req: Request, res: Response) => {
+//      if (!req.body.name || !req.body.youtubeUrl) {
+//         res.send(400)
+//     }
+//     const bloggerId = +req.params.id
+//     const blogger = bloggers.find(b => b.id === bloggerId)
+//     if (blogger) {
+//         blogger.name = req.body.name
+//         blogger.youtubeUrl = req.body.youtubeUrl
+//         res.status(204).send(blogger)
+//     } else {
+//         res.status(404).send({errorsMessages: [{message: "string", field: "title"}], resultCode: 1})
+//     }
+// })
+// app.delete('/bloggers/:id', (req, res) => {
+//     const bloggerId = +req.params.id
+//     const filteredBloggers = bloggers.filter(b => b.id !== bloggerId)
+//     if(filteredBloggers){
+//         res.status(204).send(filteredBloggers)
+//     } else {
+//         res.send(404)
+//     }
+// })
 
 app.listen(PORT, () => {
     console.log(`listening ${PORT} port`)
